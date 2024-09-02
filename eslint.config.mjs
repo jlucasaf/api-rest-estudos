9648a8d2fdae2653e19d8b900e9bcc3dc015b90e
F4908484 Joao Lucas Araujo Franca <joao.franca@bb.com.br>
2024-09-02 09:49:29 -0300

    eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,6 +4,16 @@ import tseslint from "typescript-eslint";
 
 
 export default [
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    languageOptions: { globals: globals.browser },
+    rules: {
+      "semi": ["error", "always"], // Exige ponto e vírgula no final das linhas
+      "quotes": ["error", "double"], // Exige aspas duplas
+      "no-unused-vars": ["warn"], // Emite um aviso para variáveis não utilizadas
+      // Adicione mais regras conforme necessário
+    }
+  },
   {files: ["**/*.{js,mjs,cjs,ts}"]},
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
